27a961613d7fd43c14369b71a0e126a458c26472
Muhammad Aminudin <aminudin199809@gmail.com>
2021-12-11 13:30:58 +0700

    Tugas 3 Js
    Mohon untuk di review

---
+++ tugas3.js
@@ -0,0 +1,12 @@
+function tanya() {
+  sapa = "Hallo apa kabar";
+
+  return sapa;
+}
+console.log(tanya());
+
+function jawab(x, y) {
+  console.log(x);
+  console.log(y);
+}
+jawab("Alhamdulillah", "Sehat");
